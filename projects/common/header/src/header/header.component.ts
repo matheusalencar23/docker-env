571dcedb5c956d3230d1809936f30b02ed33e012
Matheus Alencar <matheusalencar6942@gmail.com>
2023-09-02 11:34:58 -0300

    adicionado header compartilhado nos três projetos

--- projects/common/header/src/header/header.component.ts
+++ projects/common/header/src/header/header.component.ts
@@ -2,17 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-header',
+  selector: 'ds-header',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      header works!
-    </p>
-  `,
-  styles: [
-  ]
+  template: ` <p>header works!</p> `,
+  styles: [],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
--- projects/common/header/src/public-api.ts
+++ projects/common/header/src/public-api.ts
@@ -1,6 +1,1 @@
-/*
- * Public API Surface of header
- */
-
-export * from './lib/header.service';
-export * from './lib/header.component';
+export * from './header/header.component';
